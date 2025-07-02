a3b76a77193fd6ee8c8f2e3bf2fad3e7cd15c8ce
Durgvijay Tiwari <durgvijaytiwari82@gmail.com>
2025-07-02 18:54:29 +0530

    fix: backend URL

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL], // ✅ as array
+    origin: [process.env.FRONTEND_URL]|| "localhost:5173", // ✅ as array
  
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
