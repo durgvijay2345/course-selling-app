fad584c71b9165778f90fb453a8052e4998144be
Durgvijay Tiwari <durgvijaytiwari82@gmail.com>
2025-07-02 15:53:57 +0530

    fix: updated cors origin to array

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,  // ✅ Use array
+    origin: [process.env.FRONTEND_URL], // ✅ as array
+ 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
